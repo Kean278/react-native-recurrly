0a558357cd041a685ef6fb75b2ebf6a8830ba3a5
kean <keykamg@gmail.com>
2026-09-06 12:12:43 +0200

    feat: project setupo, nativewind, routing, tab navigation

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,17 @@
 import "@/global.css";
 import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "nativewind";
+const  SafeAreaView = styled(RNSafeAreaView);
+
 
 export default function App() {
   return (
-      <View className="flex-1 items-center justify-center bg-green-50">
-        <Text className="font-bold text-success">
-          Welcome to a!
+      <SafeAreaView className="flex-1 bg-background p-5">
+        <Text className="text-xl font-bold text-success">
+            Welcome to the app!!!
         </Text>
-          <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4" >
-            Go to OnBoarding
-          </Link>
-          <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4" >
-              Sing OUT
-          </Link>
 
-      </View>
+      </SafeAreaView>
   );
 }
--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,9 +1,15 @@
+import {View, Text} from 'react-native'
 import React from 'react'
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "nativewind";
+const SafeAreaView = styled(RNSafeAreaView);
 
-function Insights() {
+const Insights = () => {
     return (
-        <div>Insights</div>
+        <SafeAreaView className="flex-1 bg-background p-5">
+            <Text> Insights</Text>
+        </SafeAreaView>
     )
 }
 
-export default Insights
+export default Insights;
